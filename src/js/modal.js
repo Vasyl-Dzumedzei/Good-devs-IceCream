c14e063ed158e18fd047545c4d333727da310afc
Mykola <82897438+KOLT5885@users.noreply.github.com>
2021-07-19 00:02:04 +0300

    fautere-modal-1

--- src/js/modal.js
+++ src/js/modal.js
@@ -7,6 +7,10 @@
         openModalBtn4: document.querySelector('[data-modal-open-4]'),
         openModalBtn5: document.querySelector('[data-modal-open-5]'),
         closeModalBtn: document.querySelector('[data-modal-close]'),
+        closeModalBtn2: document.querySelector('[data-modal-close-2]'),
+        closeModalBtn3: document.querySelector('[data-modal-close-3]'),
+        closeModalBtn4: document.querySelector('[data-modal-close-4]'),
+        closeModalBtn5: document.querySelector('[data-modal-close-5]'),
         modal: document.querySelector('[data-modal]'),
     };
 
@@ -17,6 +21,10 @@
     refs.openModalBtn4.addEventListener('click', toggleModal);
     refs.openModalBtn5.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn2.addEventListener('click', toggleModal);
+    refs.closeModalBtn3.addEventListener('click', toggleModal);
+    refs.closeModalBtn4.addEventListener('click', toggleModal);
+    refs.closeModalBtn5.addEventListener('click', toggleModal);
 
     function toggleModal() {
         refs.modal.classList.toggle('is-hidden');
