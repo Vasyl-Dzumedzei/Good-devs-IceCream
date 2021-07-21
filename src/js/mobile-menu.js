728f3817bd9a4a191125e714c5f919b1f3e45fcb
Mykola <82897438+KOLT5885@users.noreply.github.com>
2021-07-21 22:52:51 +0300

    feature-modal-order-js-for-modals

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -4,6 +4,13 @@
     const mobileMenu = document.querySelector('.menu');
     const mobileContainer = document.querySelector('.header-container');
 
+    // Кнопки закрытия пунктами меню
+    const closeMenuBtnHero = document.querySelector('.site-list__link--hero');
+    const closeMenuBtnAbout = document.querySelector('.site-list__link--about');
+    const closeMenuBtnAbout1 = document.querySelector('.site-list__link--about-1');
+    const closeMenuBtnProducts = document.querySelector('.site-list__link--products');
+    const closeMenuBtnContacts = document.querySelector('.site-list__link--contacts');
+
     const toggleMenu = () => {
         const isMenuOpen =
             openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
@@ -20,6 +27,12 @@
     openMenuBtn.addEventListener('click', toggleMenu);
     closeMenuBtn.addEventListener('click', toggleMenu);
 
+    // события клик по кнопках меню
+    closeMenuBtnHero.addEventListener('click', toggleMenu);
+    closeMenuBtnAbout.addEventListener('click', toggleMenu);
+    closeMenuBtnAbout1.addEventListener('click', toggleMenu);
+    closeMenuBtnProducts.addEventListener('click', toggleMenu);
+    closeMenuBtnContacts.addEventListener('click', toggleMenu);
 
     // Закрываем мобильное меню на более широких экранах
     // в случае изменения ориентации устройства.
--- src/js/modal.js
+++ src/js/modal.js
@@ -10,20 +10,18 @@
         openModalBtn2: document.querySelector('[data-modal-open-2]'),
         openModalBtn3: document.querySelector('[data-modal-open-3]'),
         openModalBtn4: document.querySelector('[data-modal-open-4]'),
-        openModalBtn5: document.querySelector('[data-modal-open-5]'),
+
         //кнопки закрытия
         closeModalBtn: document.querySelector('[data-modal-close]'),
         closeModalBtn2: document.querySelector('[data-modal-close-2]'),
         closeModalBtn3: document.querySelector('[data-modal-close-3]'),
         closeModalBtn4: document.querySelector('[data-modal-close-4]'),
-        closeModalBtn5: document.querySelector('[data-modal-close-5]'),
 
         //якоря для куда будет помещен класс is-hidden
         modal: document.querySelector('[data-modal]'),
         modal2: document.querySelector('[data-modal-2]'),
         modal3: document.querySelector('[data-modal-3]'),
         modal4: document.querySelector('[data-modal-4]'),
-        modal5: document.querySelector('[data-modal-5]'),
     };
 
     //нажатия на кнопку открытия
@@ -32,13 +30,12 @@
     refs.openModalBtn2.addEventListener('click', toggleModal2);
     refs.openModalBtn3.addEventListener('click', toggleModal3);
     refs.openModalBtn4.addEventListener('click', toggleModal4);
-    refs.openModalBtn5.addEventListener('click', toggleModal5);
+
     //нажатия на кнопку закрытия
     refs.closeModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn2.addEventListener('click', toggleModal2);
     refs.closeModalBtn3.addEventListener('click', toggleModal3);
     refs.closeModalBtn4.addEventListener('click', toggleModal4);
-    refs.closeModalBtn5.addEventListener('click', toggleModal5);
 
     //добавления(удаления) класса is-hidden
     function toggleModal() {
@@ -52,9 +49,6 @@
     }
     function toggleModal4() {
         refs.modal4.classList.toggle('is-hidden');
-    }
-    function toggleModal5() {
-        refs.modal5.classList.toggle('is-hidden');
     }
 
 })();
